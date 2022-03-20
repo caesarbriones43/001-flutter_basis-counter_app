eb83a8987ed1548377567984aa689cf883043905
caesarbriones43 <caesarbriones43@gmail.com>
2022-03-20 05:19:03 -0600

    buildcontext, scaffold, columns

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,8 +5,20 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("This is my first widget"),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("CounterApp"),
+        elevation: 5.0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text('Number of Clicks'),
+            Text('0'),
+          ],
+        ),
+      ),
     );
   }
 }
