a99cf0f80d5501249141c54648dada9982f74ad6
caesarbriones43 <caesarbriones43@gmail.com>
2022-03-21 00:24:58 -0600

    refactoring code

--- lib/screens/counter_app.dart
+++ lib/screens/counter_app.dart
@@ -10,6 +10,21 @@ class CounterApp extends StatefulWidget {
 class _CounterAppState extends State<CounterApp> {
   var counter = 0;
 
+  void increase() {
+    counter++;
+    setState(() {});
+  }
+
+  void decrease() {
+    counter--;
+    setState(() {});
+  }
+
+  void reset() {
+    counter = 0;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     var fontSize30 = const TextStyle(fontSize: 30);
@@ -20,34 +35,10 @@ class _CounterAppState extends State<CounterApp> {
         elevation: 5.0,
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: Container(
-        margin: const EdgeInsets.only(bottom: 20.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            FloatingActionButton(
-              onPressed: () {
-                counter--;
-                setState(() {});
-              },
-              child: const Icon(Icons.exposure_minus_1_outlined),
-            ),
-            FloatingActionButton(
-              onPressed: () {
-                counter = 0;
-                setState(() {});
-              },
-              child: const Icon(Icons.restore),
-            ),
-            FloatingActionButton(
-              onPressed: () {
-                counter++;
-                setState(() {});
-              },
-              child: const Icon(Icons.exposure_plus_1_outlined),
-            )
-          ],
-        ),
+      floatingActionButton: FloatingActions(
+        increaseFn: increase,
+        decreaseFn: decrease,
+        resetFn: reset,
       ),
       body: Center(
         child: Column(
@@ -57,6 +48,43 @@ class _CounterAppState extends State<CounterApp> {
             Text('$counter', style: fontSize30),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class FloatingActions extends StatelessWidget {
+  final Function increaseFn;
+  final Function decreaseFn;
+  final Function resetFn;
+
+  const FloatingActions(
+      {Key? key,
+      required this.increaseFn,
+      required this.decreaseFn,
+      required this.resetFn})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          FloatingActionButton(
+            onPressed: () => decreaseFn(),
+            child: const Icon(Icons.exposure_minus_1_outlined),
+          ),
+          FloatingActionButton(
+            onPressed: () => resetFn(),
+            child: const Icon(Icons.restore),
+          ),
+          FloatingActionButton(
+            onPressed: () => increaseFn(),
+            child: const Icon(Icons.exposure_plus_1_outlined),
+          )
+        ],
       ),
     );
   }
